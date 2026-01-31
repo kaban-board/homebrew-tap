39b789543d8263897ec3def1d1669eab74b65c1c
Aleksandr Beshkenadze <beshkenadze@gmail.com>
2026-01-31 15:04:49 +0200

    kaban 0.3.4
    Automated update from https://github.com/kaban-board/kaban

--- Formula/kaban.rb
+++ Formula/kaban.rb
@@ -1,8 +1,8 @@
 class Kaban < Formula
   desc "Terminal Kanban for AI Code Agents"
   homepage "https://beshkenadze.github.io/kaban"
-  url "https://github.com/kaban-board/kaban/releases/download/v0.3.3/kaban-darwin-arm64-0.3.3.tar.gz"
-  sha256 "e60aee6e383fca8aa473d41fbfa0bee029604174026d9e1c423ca69ead5c63e9"
+  url "https://github.com/kaban-board/kaban/releases/download/v0.3.4/kaban-darwin-arm64-0.3.4.tar.gz"
+  sha256 "97a615bac30dda4cde18ee4935280665d39310083320ddeb92ebdf3a263d937c"
   license "MIT"
 
   depends_on arch: :arm64
